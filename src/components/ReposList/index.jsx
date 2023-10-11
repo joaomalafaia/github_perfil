61d4c8b7f630194b1205bf4bffad533351ee44a3
João Malafaia <j.malafaia02@gmail.com>
2023-10-11 11:37:46 -0300

    mensagem de erro adicionada

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -4,22 +4,33 @@ import styles from './ReposList.module.css'
 const ReposList = ({ nomeUsuário }) => {
     const [repos, setRepos] = useState([])
     const [estáCarregando, setEstáCarregando] = useState(true)
+    const [usuárioInexistente, setUsuárioInexistente] = useState(false)
 
     useEffect(() => {
         setEstáCarregando(true)
         fetch(`https://api.github.com/users/${nomeUsuário}/repos`)
-        .then(res => res.json())
-        .then(resJson => {
-            setTimeout(() => {
-                setEstáCarregando(false)
-                setRepos(resJson)
-            }, 3000)
+        .then((res) => {
+            if (res.status === 404) {
+                setUsuárioInexistente(true);
+                setEstáCarregando(false);
+                return [];
+            } else {
+                return res.json();
+            }
         })
-    }, [nomeUsuário])
+        .then((resJson) => {
+            setTimeout(() => {
+                setEstáCarregando(false);
+                setRepos(resJson);
+            }, 3000);
+        });
+    }, [nomeUsuário]);
 
     return (
         <div className="container">
-            {estáCarregando ? (
+            {usuárioInexistente ? (
+                <h2>Usuário não encontrado, tente um usuário válido</h2>
+            ) : estáCarregando ? (
                 <h1>Carregando...</h1>
             ) : (
                 <ul className={styles.list}>
